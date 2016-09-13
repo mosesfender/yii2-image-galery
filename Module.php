fd50a515ae9e00c7809e67715878e434d32b2b6b
Moses Fender <mosesfender@gmail.com>
2016-09-13 14:56:14 +0300

    Init

---
+++ Module.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author Moses Fender <mosesfender@gmail.com>
+ */
+
+namespace mosesfender\galery;
+
+class Module extends \yii\base\Module implements \yii\base\BootstrapInterface {
+
+    public function bootstrap($app) {
+        
+    }
+
+    public function init() {
+        parent::init();
+    }
+
+}
---
+++ controllers/ConductorController.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace mosesfender\galery\controllers;
